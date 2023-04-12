dfd39a8654e11206557d499e9af6a687e823e216
kangbokyun <bk940204@naver.com>
2023-04-12 17:59:56 +0900

    SubMate Update 1.1.0 Subway-Line per lat, lng

--- src/main/java/SubMate/Service/SettingService.java
+++ src/main/java/SubMate/Service/SettingService.java
@@ -266,38 +266,34 @@ public class SettingService {
 
 	// OpenAPI 1 ~ 8 호선 역사 좌표
 	public void getTrainInfo() {
+//		String[] line1 = {"당고개", "상계", "노원", "창동", "쌍문", "수유", "미아", "미아삼거리", "길음", "성신여대입구", "한성대입구", "혜화", "동대문", "동대문역사문화공원", "충무로", "명동", "회현", "서울역", "숙대입구", "삼각지", "신용산", "이촌", "동작", "이수","사당", "남태령", "경마공원", "과천", "정부과천청사", "인덕원", "평촌", "범계", "금정", "산본", "수리산", "대야미", "반월", "상록수", "한대앞", "중앙", "고잔", "공단", "안산", "신길온천", "정왕", "오이도"};
+
+		String[] line2 = {"시청", "을지로입구", "을지로3가", "을지로4가", "동대문역사문화공원", "신당", "상왕십리", "왕십리", "한양대", "뚝섬", "성수", "건대입구", "구의", "강변", "잠실나루", "잠실", "신천", "종합운동장", "삼성", "선릉", "역삼", "강남", "교대", "서초", "방배", "사당", "낙성대", "서울대입구", "봉천", "신림", "신대방", "구로디지털단지", "대림", "신도림", "문래", "영등포구청", "당산" , "합정", "홍대입구", "신촌", "이대", "아현", "충정로", "시청"};
+		String[] line3 = {"대화", "주엽", "정발산", "마두", "백석", "대곡", "화정", "원당", "삼송", "지축", "구파발", "연신내", "불광", "녹번", "홍제", "무악재", "독립문", "경복궁", "안국", "종로3가", "을지로3가", "충무로", "동대입구", "약수", "금호", "옥수", "압구정", "신사", "잠원", "고속터미널", "교대", "남부터미널", "양재", "매봉", "도곡", "대치", "학여울", "대청", "일원", "수서", "가락시장", "경찰병원", "오금"};
+		String[] line4 = {"당고개", "상계", "노원", "창동", "쌍문", "수유", "미아", "미아삼거리", "길음", "성신여대입구", "한성대입구", "혜화", "동대문", "동대문역사문화공원", "충무로", "명동", "회현", "서울역", "숙대입구", "삼각지", "신용산", "이촌", "동작", "이수","사당", "남태령", "경마공원", "과천", "정부과천청사", "인덕원", "평촌", "범계", "금정", "산본", "수리산", "대야미", "반월", "상록수", "한대앞", "중앙", "고잔", "공단", "안산", "신길온천", "정왕", "오이도"};
+		String[] line6 = {"응암", "역촌", "불광", "독바위", "연신내", "구산", "응암", "새절", "증산", "디지털미디어시티", "월드컵경기장", "마포구청", "망원", "합정", "상수", "광흥창", "대흥", "공덕", "효창공원앞", "삼각지", "녹사평", "이태원", "한강진", "버티고개", "약수", "청구", "신당", "동묘앞", "창신", "보문", "안암", "고려대", "월곡", "상월곡", "돌곶이", "석계", "태릉입구", "화랑대", "봉화산"};
+		String[] line7 = {"장암", "도봉산", "수락산", "마들", "노원", "중계", "하계", "공릉", "태릉입구", "먹골", "중화", "상봉", "면목", "사가정", "용마산", "중곡", "군자", "어린이대공원", "건대입구", "뚝섬유원지", "청담", "강남구청", "학동", "논현", "반포", "고속터미널", "내방", "이수", "남성", "숭실대입구", "상도", "장승배기", "신대방삼거리", "보라매", "신풍", "대림", "남구로", "가산디지털단지", "철산", "광명사거리", "천왕", "온수", "까치울", "부천종합운동장", "춘의", "신중동", "부천시청", "상동", "삼산체육관", "굴포천", "부평구청"};
+		String[] line8 = {"암사", "천호", "강동구청", "몽촌토성", "잠실", "석촌", "송파", "가락시장", "문정", "장지", "복정", "산성", "남한산성입구", "단대오거리", "신흥", "수진", "모란"};
+		String[] line9 = {"개화", "김포공항", "공항시장", "신방화", "양천향교", "가양", "증미", "등촌", "염창", "신목동", "선유도", "당산", "국회의사당", "여의도", "샛강", "노량진", "노들", "흑석", "동작", "구반포", "신반포", "고속터미널", "사평", "신논현", "언주", "선정릉", "삼성중앙", "봉은사", "종합운동장"};
+		String[] newBundang = {"강남", "양재", "양재시민의숲", "청계산입구", "판교", "정자", "미금", "동천", "수지구청", "성복", "상현", "광교중앙", "광교"};
+
+		청량리 왕십리 서울숲 압구정로데오 강남구청 선정릉 선릉 한티 도곡 구룡 개포동 대모산입구 복정 가천대 태평 모란 야탑 이매 서현 수내 정자 미금 오리 죽전 보정 구성 신갈 기흥 상갈 청명 영통 망포 매탄권선 수원시청 매교 수원 고색 오목천 어천 야목 사리 한대앞 중앙</b>-<b>고잔</b>-<b>초지</b>-<b>안산</b>-<b>신길온천</b>-<b>정왕</b><b>-오이도</b>-달월-월곶-소래포구-인천논현-호구포-남동인더스파크-<b>원인재</b>-연수-송도-인하대-숭의-신포-<b>인천</b></p>
+
+		List<Map<String, String>> line_4 = new ArrayList<>();
 		try {
 			List<SubWayDTO> subWayDTOS = new ArrayList<>();
-
-			for(int a = 0; a < 12; a++) {
-				String otherLines = "";
-				if(a == 0) { // 1호선
-					otherLines = "https://api.odcloud.kr/api/15041300/v1/uddi:90c8cf16-7bc9-42a4-a219-9a54f47768ed?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 1) { // 2호선
-					otherLines = "https://api.odcloud.kr/api/15041301/v1/uddi:3ecd8bc2-34ea-4860-a788-bf2578754ad9?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 2) { // 3호선
-					otherLines = "https://api.odcloud.kr/api/15041302/v1/uddi:e654fca8-d6d5-4977-bf0d-a4ebea52d5b6?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 3) { // 4호선
-					otherLines = "https://api.odcloud.kr/api/15041303/v1/uddi:c49053c3-6900-46c9-9615-cf5cc51c0dcc?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 4) { // 5호선
-					otherLines = "https://api.odcloud.kr/api/15041304/v1/uddi:8717c1fd-0d93-465e-93fb-f34dda9612d5?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 5) { // 6호선
-					otherLines = "https://api.odcloud.kr/api/15041305/v1/uddi:e405b333-40e8-44f3-b918-d0fd7e7dd7b4?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 6) { // 7호선
-					otherLines = "https://api.odcloud.kr/api/15041306/v1/uddi:25014d49-e302-4157-a95d-b09a383a4774?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 7) { // 8호선
-					otherLines = "https://api.odcloud.kr/api/15041334/v1/uddi:d7bacdad-8a49-4d47-8d96-b3a226db4efc?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 8) { // 9호선
-					otherLines = "https://api.odcloud.kr/api/15041335/v1/uddi:515ee279-c88f-47cc-9f94-a4dac970894c?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 9) { // 인천1호선
-					otherLines = "https://api.odcloud.kr/api/15041338/v1/uddi:a7d7eb6c-dd76-44ad-8132-a9c41880a9b0?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 10) { // 인천2호선
-					otherLines = "https://api.odcloud.kr/api/15041339/v1/uddi:f9119403-9bad-4a3e-94e0-dfaddfd48f36?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 11) { // 수인분당선
-					otherLines = "https://api.odcloud.kr/api/15041336/v1/uddi:97b7dfab-9734-4b79-8b18-e5ef46f2cc12?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				} else if(a == 12) { // 신분당선
-					otherLines = "https://api.odcloud.kr/api/15041334/v1/uddi:d7bacdad-8a49-4d47-8d96-b3a226db4efc?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
-				}
+			String[][] trainAPIData = new String[][] {
+					/* 1호선 */{"15041300", "90c8cf16-7bc9-42a4-a219-9a54f47768ed"}, /* 2호선 */{"15041301", "3ecd8bc2-34ea-4860-a788-bf2578754ad9"},
+					/* 3호선 */{"15041302", "e654fca8-d6d5-4977-bf0d-a4ebea52d5b6"}, /* 4호선 */{"15041303", "c49053c3-6900-46c9-9615-cf5cc51c0dcc"},
+					/* 5호선 */{"15041304", "8717c1fd-0d93-465e-93fb-f34dda9612d5"}, /* 6호선 */{"15041305", "e405b333-40e8-44f3-b918-d0fd7e7dd7b4"},
+					/* 7호선 */{"15041306", "25014d49-e302-4157-a95d-b09a383a4774"}, /* 8호선 */{"15041334", "d7bacdad-8a49-4d47-8d96-b3a226db4efc"},
+					/* 9호선 */{"15041335", "515ee279-c88f-47cc-9f94-a4dac970894c"}, /* 인천1호선 */{"15041338", "a7d7eb6c-dd76-44ad-8132-a9c41880a9b0"},
+					/* 인천2호선 */{"15041339", "f9119403-9bad-4a3e-94e0-dfaddfd48f36"}, /* 수인분당 */{"15041336", "97b7dfab-9734-4b79-8b18-e5ef46f2cc12"},
+					/* 신분당선 */{"15041337", "256d7380-e813-4155-a286-bdcf306f111c"}
+			};
+			for(int a = 0; a < trainAPIData.length; a++) {
+				System.out.println("trainAPIData : " + trainAPIData.length + " : " + a);
+				String otherLines = "https://api.odcloud.kr/api/" + trainAPIData[a][0] + "/v1/uddi:" + trainAPIData[a][1] + "?page=1&perPage=100&serviceKey=HECNY5QN6CQD7qu%2BwHHkPELwmRonryzFFC%2F19jU5jFMu9pT34DW66NlYrxVUnw%2BwBtTQvt7RxBbPo3RR9Y9pQQ%3D%3D";
 				URL url = new URL(otherLines);
 				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
 				String resultBuffer = bufferedReader.readLine();
